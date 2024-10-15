c0d9daa4dc85bed45e7aec690c85b2f966e196d7
Edison <krylov2310@gmail.com>
2024-10-15 12:50:13 +0500

    Update module_13_2_1.py

--- module_13_2_1.py
+++ module_13_2_1.py
@@ -9,30 +9,34 @@ Phiton 3.12
 aiogram 3.13.1
 """
 
-token = input('Введите ваш токен: ')
-bot = Bot(token=token)
+user_token = input('Введите ваш токен: ')
+bot = Bot(token=user_token)
 dp = Dispatcher()
 
 
 @dp.message(Command("help"))
 async def start_message(message: types.Message):
-    await message.answer('Домашнее задание по теме "Хендлеры обработки сообщений"'
-                         '\nЦель: написать простейшего телеграм-бота, используя асинхронные функции.'
-                         '\nЗадача "Бот поддержки (Начало):"'
-                         '\nСтудент Крылов Эдуард Васильевич.'
-                         '\nДата работы над заданием: 15.10.2024г.')
+    text = ('Домашнее задание по теме "Хендлеры обработки сообщений"'
+            '\nЦель: написать простейшего телеграм-бота, используя асинхронные функции.'
+            '\nЗадача "Бот поддержки (Начало):"'
+            '\nСтудент Крылов Эдуард Васильевич.'
+            '\nДата работы над заданием: 15.10.2024г.')
+    await message.answer(text)
+    print(text)
 
 
 @dp.message(Command("start"))
 async def start_message(message: types.Message):
-    await message.answer("Привет! Я бот помогающий твоему здоровью.")
-    print('Привет! Я бот помогающий твоему здоровью.')
+    text = "Привет! Я бот помогающий твоему здоровью."
+    await message.answer(text)
+    print(text)
 
 
 @dp.message()
 async def all_message(message):
-    await message.answer("Введите команду /start, или /help чтобы начать общение.")
-    print('Введите команду /start, или /help чтобы начать общение.')
+    text = "Введите команду /start, или /help чтобы начать общение."
+    await message.answer(text)
+    print(text)
 
 
 async def main():
